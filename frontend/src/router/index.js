87898e6946287b31dcbb74711b455e768ef4ecf6
Igor Kretov <knigor1337@gmail.com>
2024-11-22 14:55:26 +0300

    add fonts

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AuthView from '@/views/AuthView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import ErrorPage from '@/views/ErrorPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +11,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/auth',
+      name: 'auth',
+      component: AuthView,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView,
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'error',
+      component: ErrorPage,
     },
   ],
 })
