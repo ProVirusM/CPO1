1ceff31feb8a4bd7e2a72b61656cec54b63c5649
Igor Kretov <knigor1337@gmail.com>
2024-11-23 23:16:25 +0300

    added sport filter

--- frontend/src/components/EventsPage.vue
+++ frontend/src/components/EventsPage.vue
@@ -1,53 +1,93 @@
 <template>
   <div class="w-auto p-5 flex flex-col gap-4">
     <div class="flex flex-wrap gap-2 pl-20 pr-20">
-      <SelectWithSearch text="Вид спорта"></SelectWithSearch>
-      <SelectWithSearch :items="['Физра','Бег трусцой','Абоба', 'Климсаныч']" v-model="model" text="Дисциплина"></SelectWithSearch>
-      <SelectWithSearch :items="['Физра','Бег трусцой','Абоба', 'Климсаныч']" v-model="model2" text="Программа"></SelectWithSearch>
+      <SelectWithSearch
+        :items="filterStore.sport"
+        v-model="model3"
+        text="Вид спорта"
+      ></SelectWithSearch>
+      <SelectWithSearch
+        :items="['Физра', 'Бег трусцой', 'Абоба', 'Климсаныч']"
+        v-model="model"
+        text="Дисциплина"
+      ></SelectWithSearch>
+      <SelectWithSearch
+        :items="['Физра', 'Бег трусцой', 'Абоба', 'Климсаныч']"
+        v-model="model2"
+        text="Программа"
+      ></SelectWithSearch>
       <SelectWithSearch text="Место проведения"></SelectWithSearch>
       <SelectWithSearch text="Пол, возрастная группа"></SelectWithSearch>
       <SelectWithSearch text="Сроки проведения"></SelectWithSearch>
       <SelectWithSearch text="Тип"></SelectWithSearch>
-      <ParticipantsPicker v-model:start="amountStart" v-model:end="amountEnd" text="Количество участников"></ParticipantsPicker>
+      <ParticipantsPicker
+        v-model:start="amountStart"
+        v-model:end="amountEnd"
+        text="Количество участников"
+      ></ParticipantsPicker>
       <div class="w-128 flex items-center">
-        <VueDatePicker placeholder="Выберите интервал дат" width="200px" ref="dp" v-model="dateRange" locale="ru" :range="true" :enable-time-picker="false">
+        <VueDatePicker
+          placeholder="Выберите интервал дат"
+          width="200px"
+          ref="dp"
+          v-model="dateRange"
+          locale="ru"
+          :range="true"
+          :enable-time-picker="false"
+        >
           <template #action-buttons>
-            <p class="bg-[#00DB62] p-2 rounded-xl font-bold" @click="selectDate">Выбрать интервал</p>
+            <p class="bg-[#00DB62] p-2 rounded-xl font-bold" @click="selectDate">
+              Выбрать интервал
+            </p>
           </template>
         </VueDatePicker>
       </div>
-      <button @click="modalSubscribeVisible = true" class="bg-[#312E5C] p-2 pt-1 pb-1 rounded-xl text-xl text-[#F7F7F7] font-bold flex gap-2 items-center active:bg-[#292650]">
+      <button
+        @click="modalSubscribeVisible = true"
+        class="bg-[#312E5C] p-2 pt-1 pb-1 rounded-xl text-xl text-[#F7F7F7] font-bold flex gap-2 items-center active:bg-[#292650]"
+      >
         <Bell></Bell>
         Подписаться
       </button>
-      <ModalSubscribe v-model="modalSubscribeVisible"/>
+      <ModalSubscribe v-model="modalSubscribeVisible" />
     </div>
-    
+
     <CalendarPage></CalendarPage>
   </div>
 </template>
 
 <script setup>
-  import CalendarPage from './CalendarPage.vue';
-  import SelectWithSearch from './common/SelectWithSearch.vue';
-  import ModalSubscribe from './common/ModalSubscribe.vue';
-  import VueDatePicker from '@vuepic/vue-datepicker';
-  import { Bell } from 'lucide-vue-next';
-  import { ref } from 'vue';
-  import ParticipantsPicker from './common/ParticipantsPicker.vue';
-  
-  const model = ref([])
-  const model2 = ref([])
-  const dateRange = ref([])
-  const modalSubscribeVisible = ref(false)
-  const amountStart = ref(0)
-  const amountEnd = ref(10000)
+import CalendarPage from './CalendarPage.vue'
+import SelectWithSearch from './common/SelectWithSearch.vue'
+import ModalSubscribe from './common/ModalSubscribe.vue'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import { Bell } from 'lucide-vue-next'
+import { ref } from 'vue'
+import ParticipantsPicker from './common/ParticipantsPicker.vue'
+import { useFilterStore } from '@/stores/filterStore'
+import { useLoginStore } from '@/stores/loginStore'
+import { onMounted } from 'vue'
 
-  const dp = ref();
+const model = ref([])
+const model2 = ref([])
+const model3 = ref([])
+const dateRange = ref([])
+const modalSubscribeVisible = ref(false)
+const amountStart = ref(0)
+const amountEnd = ref(10000)
 
-  const selectDate = () => {
-    dp.value.selectDate();
-  }
+const filterStore = useFilterStore()
+
+onMounted(async () => {
+  await filterStore.getSports()
+  console.log(filterStore.sport.slice(0, 5))
+})
+
+const dp = ref()
+
+const selectDate = () => {
+  dp.value.selectDate()
+}
 </script>
 
 <style lang="scss" scoped></style>
--- frontend/src/stores/filterStore.js
+++ frontend/src/stores/filterStore.js
@@ -2,13 +2,39 @@ import { defineStore } from 'pinia'
 import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import axios from 'axios'
+import { useLoginStore } from './loginStore'
+import { useToast } from 'vue-toastification'
 
 export const useFilterStore = defineStore('filter', () => {
   const sport = ref([])
 
+  const toast = useToast()
+
+  const loginStore = useLoginStore()
+
+  console.log(loginStore.headers)
+
   const router = useRouter()
+
+  async function getSports() {
+    return axios
+      .get(import.meta.env.VITE_BASE_URL + '/api/sports', { headers: loginStore.headers })
+      .then((response) => {
+        console.log(response.data)
+        sport.value = response.data.data
+        return response.data
+      })
+      .catch((error) => {
+        if (error.status === 401) {
+          router.push({ name: 'auth' })
+          toast.error('Пользователь не авторизован!')
+        }
+        return false
+      })
+  }
 
   return {
     sport,
+    getSports,
   }
 })
